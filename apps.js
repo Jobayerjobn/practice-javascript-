b7deff53bb0d0e55d6fbe37b6c95a7e82e66d9b0
jobayerjobn <jobayerjoban0048@gamil.com>
2024-07-03 10:28:32 +0600

    module system

---
+++ apps.js
@@ -0,0 +1,49 @@
+//  named import 
+
+// import {name, age, email} from './index.js';
+// console.log(name, age, email);
+
+// import {name, age, email} from './index.js';
+// console.log(name, email, age);
+// import{name, age, email} from './index.js';
+// console.log(name,email, age);
+// import {name, email, age } from './index.js';
+// console.log(name, age, email);
+
+// import {name as hello, email as how, age as dine} from './index.js';
+// console.log(hello, how, dine);
+
+
+
+
+// All file export and import;
+
+// import * as hello from './index.js';
+// console.log(hello);
+
+// import * as external from './index.js';
+// console.log(external);
+// import * as how from './index.js';
+// console.log(how);
+// import * as him from './index.js';
+// console.log(him);
+// import * as internal from './index.js';
+// console.log(internal);
+
+
+
+// Last step:
+// Default import and export;
+// import default {name, email, age} from './index.js';
+// console.log(default);
+//  There are three types of module system in  the javascript;
+// Number 1. Named export and Import;
+// Number 2. default export and Import;
+// Number 3. all file export and Import;
+
+// Number 1. 
+export const name = 'Jobayer';
+export const age = 19;
+const email = 'jobayerjoban0048@gmail.com';
+
+export default email;
---
+++ index.js
@@ -0,0 +1,44 @@
+// module system
+// There are three module system in javascript;
+//1. Named import and Named Export;
+// 2. default import and default Export;
+// 3.All file import and export;
+
+
+// number 1 named export;
+
+// export const name = 'Jobayer';
+// export const age = 19;
+// export const email = 'jobayerjoban0048@gmail.com';
+// // const hello = 'How';
+// // export default hello;
+
+// export default function myFunc(){
+
+//     console.log('Hello I am myFunc');
+// import {name, age as boyos, email} from './apps.js';
+// console.log(name, boyos, email);
+// import {name as num, age as boyos, email as massage} from "./apps.js";
+// console.log(num, boyos, massage);
+
+// Number 2  default export and import;
+
+// import how, {name, age} from './apps.js';
+// console.log(how, name, age);
+// import hello , {name, age} from './apps.js';
+// console.log(hello, name, age);
+// import why , {name, age} from './apps.js';
+// console.log(why, name, age);
+
+// Number  3;
+
+// import * as hello from './apps.js';
+// console.log(hello);
+// import * as how from './apps.js';
+// console.log(how);
+// import * as why from './apps.js';
+// console.log(why);
+// import * what from './apps.js';
+// console.log(what);
+// import * where from './apps.js';
+// console.log(where);
